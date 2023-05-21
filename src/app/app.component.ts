b036f8247d2f7261b416fc9ffa3fcbdaf9657cfa
DanielTusarma <118632587+DanielTusarma@users.noreply.github.com>
2023-05-21 17:38:51 -0500

    Inicio guia de guards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { TokenService } from './servicios/token.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'unimarket-frontend';
+  isLogged = false;
+  email:string = "";
+  constructor(private tokenService:TokenService) { }
+  ngOnInit(): void {
+      const objeto = this;
+      this.sesionService.currentMessage.subscribe({
+      next: data => {
+      objeto.actualizarSesion(data);
+      }
+      });
+      this.actualizarSesion(this.tokenService.isLogged());
+      }
+      private actualizarSesion(estado: boolean) {
+      this.isLogged = estado;
+      if (estado) {
+      this.email = this.tokenService.getEmail();
+      }else{
+        this.email = "";
+}
+}
+}
+public logout(){
+this.tokenService.logout();
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: UsuarioInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
